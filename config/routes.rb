1856a3d94fb7d11af300b85dbe166ba620d1a311
Piotr Jendrzyca <pjendrzyca@forefrontcorp.com>
2017-10-30 21:33:40 +0100

    models added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  resources :tags
+  resources :books
+  root 'books#index'
 end
